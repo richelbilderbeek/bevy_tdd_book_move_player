08bc64b6eeff6b15ea4223a9d25e0825c69629f1
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-15 07:02:12 +0200

    Use 'fn' for a function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod app;
 fn main() {
     let velocity = Vec2::new(0.2, 0.1);
     let mut app = create_app(velocity);
-    let add_camera_fun = |mut commands: Commands| {
+    let add_camera_fn = |mut commands: Commands| {
         commands.spawn(Camera2dBundle::default());
     };
     app.add_systems(Startup, add_camera_fun);
