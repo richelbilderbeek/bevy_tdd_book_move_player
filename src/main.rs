b9a6803a4bf1f48a0b49889872c3bf45c839e9ca
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-21 13:09:31 +0100

    'Camera2dBundle' is now called 'Camera2d'

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let velocity = Vec2::new(0.2, 0.1);
     let mut app = create_app(velocity);
     let add_camera_fn = |mut commands: Commands| {
-        commands.spawn(Camera2dBundle::default());
+        commands.spawn(Camera2d::default());
     };
     app.add_systems(Startup, add_camera_fn);
     app.add_plugins(DefaultPlugins);
